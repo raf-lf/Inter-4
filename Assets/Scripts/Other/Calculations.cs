42b096d73c4420ceb1ce2174e38464914c43c40b
Rafael Farinha <rafaelzimrp@yahoo.com>
2021-10-27 18:08:23 -0300

    NÃO SEI

--- Assets/Scripts/Other/Calculations.cs
+++ Assets/Scripts/Other/Calculations.cs
@@ -6,20 +6,17 @@ public static class Calculations
 {
     public static float GetRotationZToTarget(Vector2 origin, Vector2 target)
     {
-        float rotationZ;
         Vector2 midPoint = target - origin;
-        rotationZ = Mathf.Atan2(midPoint.y, midPoint.x) * Mathf.Rad2Deg;
+        float rotationZ = Mathf.Atan2(midPoint.y, midPoint.x) * Mathf.Rad2Deg;
 
         return rotationZ;
     }
 
     public static Vector2 GetDirectionToTarget(Vector2 origin, Vector2 target)
     {
-        Vector2 direction;
-
         Vector2 midPoint = target - origin;
         float distance = midPoint.magnitude;
-        direction = midPoint / distance;
+        Vector2 direction = midPoint / distance;
         direction.Normalize();
 
         return direction;
